70f6c5645e9589d420aa2e19be175b2d2b102a23
Erik <erikwestberg7@gmail.com>
2022-11-04 14:29:09 +0100

    Added SharpHorizons.Samples

---
+++ SharpHorizons/SharpHorizons.Samples/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using SharpHorizons;
+using SharpHorizons.Vectors;
+
+using System;
+
+internal class Program
+{
+    public static void Main()
+    {
+        var host = Host.CreateDefaultBuilder().ConfigureServices(ConfigureServices).Build();
+
+        host.Start();
+
+        var uh = host.Services.GetService<IVectorsQueryFactory>();
+
+        Console.WriteLine(uh?.Fluent());
+    }
+
+    private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+    {
+        services.AddSharpHorizons();
+    }
+}
