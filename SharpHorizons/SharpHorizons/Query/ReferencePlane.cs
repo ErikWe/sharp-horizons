18879d8c271ff3e12132e9bbe9f54e01327be934
Erik <erikwestberg7@gmail.com>
2022-11-05 00:10:23 +0100

    Minor code-style fixes

--- SharpHorizons/SharpHorizons/Query/ReferencePlane.cs
+++ SharpHorizons/SharpHorizons/Query/ReferencePlane.cs
@@ -1,6 +1,6 @@
 ﻿namespace SharpHorizons.Query;
 
-/// <summary>Specifies the reference plane used to express orbital state vectors and orbital elements. Used together with <see cref="ReferenceSystem"/> to determine the coordinate basis.</summary>
+/// <summary>Specifies the reference plane used to express orbital state vectors and osculating orbital elements. Used together with <see cref="ReferenceSystem"/> to determine the coordinate basis.</summary>
 public enum ReferencePlane
 {
     /// <summary>The ecliptic, the orbit of Earth around the Sun.</summary>
