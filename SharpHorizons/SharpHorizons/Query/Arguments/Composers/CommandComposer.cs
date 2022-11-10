f8f610afe795dcb9a36e12016974e6415905dd9a
Erik <erikwestberg7@gmail.com>
2022-11-10 04:26:56 +0100

    Added implemention of ICommandComposer<QueryCommand>

---
+++ SharpHorizons/SharpHorizons/Query/Arguments/Composers/CommandComposer.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpHorizons.Query.Arguments.Composers;
+
+using SharpHorizons.Query;
+using SharpHorizons.Query.Arguments;
+
+using System.ComponentModel;
+
+/// <summary>Composes <see cref="ICommandArgument"/> that describe <see cref="QueryCommand"/>.</summary>
+internal sealed class CommandComposer : ICommandComposer<QueryCommand>
+{
+    ICommandArgument IArgumentComposer<ICommandArgument, QueryCommand>.Compose(QueryCommand obj) => new QueryArgument(obj switch
+    {
+        QueryCommand.Ephemerides => throw new ComposeEphemeridesQueryCommandException(),
+        QueryCommand.MajorBodyList => "MB",
+        QueryCommand.News => "NEWS",
+        _ => throw new InvalidEnumArgumentException(nameof(obj), (int)obj, typeof(QueryCommand))
+    });
+}
---
+++ SharpHorizons/SharpHorizons/Query/Arguments/Composers/ComposeEphemeridesQueryCommandException.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpHorizons.Query.Arguments.Composers;
+
+using SharpHorizons.Query.Target;
+
+using System;
+
+/// <summary>Represents an error caused by attempting to compose <see cref="QueryCommand.Ephemerides"/> as a <see cref="ICommandArgument"/>.</summary>
+public sealed class ComposeEphemeridesQueryCommandException : Exception
+{
+    /// <inheritdoc cref="ComposeEphemeridesQueryCommandException"/>
+    public ComposeEphemeridesQueryCommandException() : base($"{QueryCommand.Ephemerides} cannot be used to compose a {typeof(ICommandArgument).FullName}. Instead use the {typeof(ITarget).FullName}.") { }
+
+    /// <inheritdoc cref="ComposeEphemeridesQueryCommandException"/>
+    /// <param name="message">The message that describes the error.</param>
+    public ComposeEphemeridesQueryCommandException(string? message) : base(message) { }
+
+    /// <inheritdoc cref="ComposeEphemeridesQueryCommandException"/>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">The <see cref="Exception"/> that caused the current exception.</param>
+    public ComposeEphemeridesQueryCommandException(string? message, Exception? innerException) : base(message, innerException) { }
+}
