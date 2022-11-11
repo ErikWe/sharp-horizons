28711cb346290585dd8ef806b0b7612e5d47d18e
Erik <erikwestberg7@gmail.com>
2022-11-11 16:17:28 +0100

    Added IURIComposer w/ implementation

---
+++ SharpHorizons/SharpHorizons.Abstractions/Query/Composing/IURIComposer.cs
@@ -0,0 +1,11 @@
+﻿namespace SharpHorizons.Query.Composing;
+
+using System;
+
+/// <summary>Composes <see cref="Uri"/> describing <see cref="HorizonsQueryString"/>.</summary>
+public interface IURIComposer
+{
+    /// <summary>Composes a <see cref="Uri"/> describing <paramref name="query"/>.</summary>
+    /// <param name="query">The composed <see cref="Uri"/> describes this <see cref="HorizonsQueryString"/>.</param>
+    public abstract Uri Compose(HorizonsQueryString query);
+}
---
+++ SharpHorizons/SharpHorizons/Query/Composing/URIComposer.cs
@@ -0,0 +1,14 @@
+﻿namespace SharpHorizons.Query.Composing;
+
+using SharpHorizons.Query;
+
+using System;
+
+/// <inheritdoc cref="IURIComposer"/>
+internal sealed class URIComposer : IURIComposer
+{
+    /// <summary>The URI of the Horizons API.</summary>
+    private Uri HorizonsAPIAddress { get; } = new("""https://ssd.jpl.nasa.gov/api/horizons.api?""");
+
+    Uri IURIComposer.Compose(HorizonsQueryString query) => new(HorizonsAPIAddress, $"?{query}");
+}
