05149623d99f7f781bd5025a3de690ebc8319457
Erik <erikwestberg7@gmail.com>
2022-11-20 22:24:19 +0100

    Added MPCComet

---
+++ SharpHorizons/SharpHorizons.Domain/Identity/MPCComet.cs
@@ -0,0 +1,34 @@
+﻿namespace SharpHorizons.Identity;
+
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>Represents a comet in the MPC catalogue.</summary>
+public sealed record class MPCComet
+{
+    /// <summary>The <see cref="MPCCometDesignation"/> that was assigned to the comet.</summary>
+    public required MPCCometDesignation Designation { get; init; }
+
+    /// <summary>The <see cref="MPCCometName"/> of the comet, or <see langword="null"/> if the comet is unnamed.</summary>
+    public MPCCometName? Name { get; init; }
+
+    /// <inheritdoc cref="MPCComet"/>
+    public MPCComet() { }
+
+    /// <inheritdoc cref="MPCComet"/>
+    /// <param name="designation"><inheritdoc cref="Designation" path="/summary"/></param>
+    [SetsRequiredMembers]
+    public MPCComet(MPCCometDesignation designation)
+    {
+        Designation = designation;
+    }
+
+    /// <inheritdoc cref="MPCComet"/>
+    /// <param name="designation"><inheritdoc cref="Designation" path="/summary"/></param>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+    [SetsRequiredMembers]
+    public MPCComet(MPCCometDesignation designation, MPCCometName? name)
+    {
+        Designation = designation;
+        Name = name;
+    }
+}
---
+++ SharpHorizons/SharpHorizons.Domain/Identity/MPCCometName.cs
@@ -0,0 +1,37 @@
+﻿namespace SharpHorizons.Identity;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>Represents the name of a comet in the MPC catalogue.</summary>
+public readonly record struct MPCCometName
+{
+    /// <summary>The name of the comet in the MPC catalogue.</summary>
+    public required string Name { get; init; }
+
+    /// <inheritdoc cref="MPCCometName"/>
+    public MPCCometName() { }
+
+    /// <inheritdoc cref="MPCCometName"/>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+    [SetsRequiredMembers]
+    public MPCCometName(string name)
+    {
+        Name = name;
+    }
+
+    /// <summary>Retrieves the name represented by <see langword="this"/>.</summary>
+    public override string ToString() => Name;
+
+    /// <summary>Retrieves the name represented by <see langword="this"/>, formatted according to <paramref name="provider"/>.</summary>
+    /// <param name="provider">Provides culture-specific formatting information.</param>
+    public string ToString(IFormatProvider? provider) => Name.ToString(provider);
+
+    /// <inheritdoc cref="MPCCometName"/>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+    public static implicit operator MPCCometName(string name) => new(name);
+
+    /// <summary>Retrieves the name represented by <paramref name="name"/>.</summary>
+    /// <param name="name"><inheritdoc cref="MPCCometName" path="/summary"/></param>
+    public static implicit operator string(MPCCometName name) => name.Name;
+}
