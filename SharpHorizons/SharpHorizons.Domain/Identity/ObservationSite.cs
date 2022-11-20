3870b12e519957f44a3160de875172baaff857ff
Erik <erikwestberg7@gmail.com>
2022-11-20 22:24:34 +0100

    Added ObservationSite and ObservationSiteName

---
+++ SharpHorizons/SharpHorizons.Domain/Identity/ObservationSite.cs
@@ -0,0 +1,26 @@
+﻿namespace SharpHorizons.Identity;
+
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>Represents a pre-defined observation site in Horizons.</summary>
+public sealed record class ObservationSite
+{
+    /// <summary>The ID of the observation site.</summary>
+    public required ObservationSiteID ID { get; init; }
+
+    /// <summary>The name of the observation site.</summary>
+    public required ObservationSiteName Name { get; init; }
+
+    /// <inheritdoc cref="ObservationSite"/>
+    public ObservationSite() { }
+
+    /// <inheritdoc cref="ObservationSite"/>
+    /// <param name="id"><inheritdoc cref="ID" path="/summary"/></param>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+    [SetsRequiredMembers]
+    public ObservationSite(ObservationSiteID id, ObservationSiteName name)
+    {
+        ID = id;
+        Name = name;
+    }
+}
