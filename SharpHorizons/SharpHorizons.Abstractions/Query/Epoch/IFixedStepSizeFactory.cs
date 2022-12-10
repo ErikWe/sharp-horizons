828cba0302e260e1a3b060599e6c48ae4681ed15
Erik <erikwestberg7@gmail.com>
2022-12-10 12:59:01 +0100

    Split IStepSizeFactory-file in one file per interface

---
+++ SharpHorizons/SharpHorizons.Abstractions/Query/Epoch/IFixedStepSizeFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpHorizons.Query.Epoch;
+
+using SharpMeasures;
+
+using System;
+
+/// <summary>Handles construction of <see cref="IFixedStepSize"/>, describing the <see cref="IStepSize"/> in a query using a fixed <see cref="Time"/>.</summary>
+public interface IFixedStepSizeFactory
+{
+    /// <summary>Describes the <see cref="IStepSize"/> in a query using a fixed <see cref="Time"/> difference <paramref name="deltaTime"/>.</summary>
+    /// <param name="deltaTime"><inheritdoc cref="IFixedStepSize.DeltaTime" path="/summary"/></param>
+    /// <exception cref="ArgumentException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public abstract IFixedStepSize Create(Time deltaTime);
+}
