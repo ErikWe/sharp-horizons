ed511fc1e9704428eb07f11c56a5aa65be678bba
Erik <erikwestberg7@gmail.com>
2022-12-16 14:42:37 +0100

    Ephemeris-related XML docs improvements

--- SharpHorizons/SharpHorizons.Ephemeris/Ephemeris/IEphemeris.cs
+++ SharpHorizons/SharpHorizons.Ephemeris/Ephemeris/IEphemeris.cs
@@ -3,7 +3,7 @@
 using System;
 using System.Collections.Generic;
 
-/// <summary>Represents an ephemeris of an object, with each <see cref="IEphemerisEntry"/> described as an instance of <typeparamref name="TEntry"/>. This is a list of <typeparamref name="TEntry"/>, ordered by <see cref="IEphemerisEntry.Epoch"/> (earliest first).</summary>
+/// <summary>Represents an ephemeris of an object, with each <see cref="IEphemerisEntry"/> described as an instance of <typeparamref name="TEntry"/>. This is a list of <typeparamref name="TEntry"/>, ordered by their <see cref="IEphemerisEntry.Epoch"/> (earliest first).</summary>
 /// <typeparam name="TEntry">The <see cref="IEphemeris{TEntry}"/> uses instances of this type to describe the properties of an object at different <see cref="IEpoch"/>.</typeparam>
 public interface IEphemeris<TEntry> : IReadOnlyList<TEntry> where TEntry : IEphemerisEntry
 {
