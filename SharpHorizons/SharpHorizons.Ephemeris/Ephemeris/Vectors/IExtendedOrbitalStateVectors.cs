50a3b93809929b0c606e57ffc024b139a1853af0
Erik <erikwestberg7@gmail.com>
2022-12-16 14:43:12 +0100

    Added ExtendedOrbitalStateVectors to represent full vectors query result

---
+++ SharpHorizons/SharpHorizons.Ephemeris/Ephemeris/Vectors/IExtendedOrbitalStateVectors.cs
@@ -0,0 +1,7 @@
+﻿namespace SharpHorizons.Ephemeris.Vectors;
+
+using SharpMeasures;
+
+/// <summary>Represents the orbital state vectors (<see cref="Position3"/> and <see cref="Velocity3"/>) of an object at some <see cref="IEpoch"/>, combined with <see cref="Distance"/>-related properties of the object,
+/// and the uncertainty in the <see cref="Position3"/> of the object.</summary>
+public interface IExtendedOrbitalStateVectors : IPositionEphemerisEntry, IVelocityEphemerisEntry, IDistanceEphemerisEntry, IPositionalUncertaintyACN, IPositionalUncertaintyPOS, IPositionalUncertaintyRTN, IPositionalUncertaintyXYZ { }
