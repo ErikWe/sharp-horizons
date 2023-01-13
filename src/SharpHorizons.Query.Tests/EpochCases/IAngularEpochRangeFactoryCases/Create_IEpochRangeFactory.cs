b3eeb8230777307c1c7512311695783c894ec084
ErikWe <erikwestberg7@gmail.com>
2023-01-13 13:02:15 +0100

    Refactored previously added test-cases

--- src/SharpHorizons.Query.Tests/EpochCases/IAngularEpochRangeFactoryCases/Create_IEpochRangeFactory.cs
+++ src/SharpHorizons.Query.Tests/EpochCases/IAngularEpochRangeFactoryCases/Create_IEpochRangeFactory.cs
@@ -1,117 +1,32 @@
 ﻿namespace SharpHorizons.Tests.QueryCases.EpochCases.IAngularEpochRangeFactoryCases;
 
 using SharpHorizons.Query.Epoch;
-
-using SharpMeasures;
-
-using System;
+using SharpHorizons.Tests.QueryCases.EpochCases.IEpochRangeFactoryCases;
 
 using Xunit;
 
 public class Create_IEpochRangeFactory
 {
     [Fact]
-    public void NullStartEpoch_ArgumentNullException()
-    {
-        var startEpoch = GetNullEpoch();
-        var stopEpoch = GetValidStopEpoch();
-        var stepSize = GetValidStepSize();
-
-        AnyError_TException<ArgumentNullException>(startEpoch, stopEpoch, stepSize);
-    }
+    public void NullStartEpoch_ArgumentNullException() => Executor_Create.NullStartEpoch_ArgumentNullException(GetService());
 
     [Fact]
-    public void NullStopEpoch_ArgumentNullException()
-    {
-        var startEpoch = GetValidStartEpoch();
-        var stopEpoch = GetNullEpoch();
-        var stepSize = GetValidStepSize();
-
-        AnyError_TException<ArgumentNullException>(startEpoch, stopEpoch, stepSize);
-    }
+    public void NullStopEpoch_ArgumentNullException() => Executor_Create.NullStopEpoch_ArgumentNullException(GetService());
 
     [Fact]
-    public void NullStepSize_ArgumentNullException()
-    {
-        var startEpoch = GetValidStartEpoch();
-        var stopEpoch = GetValidStopEpoch();
-        var stepSize = GetNullStepSize();
-
-        AnyError_TException<ArgumentNullException>(startEpoch, stopEpoch, stepSize);
-    }
+    public void NullStepSize_ArgumentNullException() => Executor_Create.NullStepSize_ArgumentNullException(GetService());
 
     [Fact]
-    public void StopEpochEarlierThanStartEpochAndNullStepSize_ArgumentNullException()
-    {
-        var startEpoch = GetValidStartEpoch();
-        var stopEpoch = GetEarlierStopEpoch();
-        var stepSize = GetNullStepSize();
-
-        AnyError_TException<ArgumentNullException>(startEpoch, stopEpoch, stepSize);
-    }
+    public void StopEpochEarlierThanStartEpochAndNullStepSize_ArgumentNullException() => Executor_Create.StopEpochEarlierThanStartEpochAndNullStepSize_ArgumentNullException(GetService());
 
     [Fact]
-    public void StopEpochEarlierThanStartEpoch_ArgumentException()
-    {
-        var startEpoch = GetValidStartEpoch();
-        var stopEpoch = GetEarlierStopEpoch();
-        var stepSize = GetValidStepSize();
-
-        AnyError_TException<ArgumentException>(startEpoch, stopEpoch, stepSize);
-    }
+    public void StopEpochEarlierThanStartEpoch_ArgumentException() => Executor_Create.StopEpochEarlierThanStartEpoch_ArgumentException(GetService());
 
     [Fact]
-    public void StopEpochSameAsStartEpoch_ArgumentException()
-    {
-        var startEpoch = GetValidStartEpoch();
-        var stopEpoch = startEpoch;
-        var stepSize = GetValidStepSize();
-
-        AnyError_TException<ArgumentException>(startEpoch, stopEpoch, stepSize);
-    }
-
-    private static void AnyError_TException<TException>(IEpoch startEpoch, IEpoch stopEpoch, IStepSize stepSize) where TException : Exception
-    {
-        var factory = GetService();
-
-        var exception = Record.Exception(() => factory.Create(startEpoch, stopEpoch, stepSize));
-
-        Assert.IsType<TException>(exception);
-    }
+    public void StopEpochSameAsStartEpoch_ArgumentException() => Executor_Create.StopEpochSameAsStartEpoch_ArgumentException(GetService());
 
     [Fact]
-    public void Valid_ExactMatch()
-    {
-        var factory = GetService();
-
-        var startEpoch = GetValidStartEpoch();
-        var stopEpoch = GetValidStopEpoch();
-        var stepSize = GetValidStepSize();
-
-        var actual = factory.Create(startEpoch, stopEpoch, stepSize);
-
-        Assert.Equal(startEpoch, actual.StartEpoch.Epoch);
-        Assert.Equal(stopEpoch, actual.StopEpoch.Epoch);
-
-        Assert.Equal(EpochSelectionMode.Range, actual.Selection);
-        Assert.Equal(EpochFormat.JulianDays, actual.Format);
-        Assert.Equal(CalendarType.Gregorian, actual.Calendar);
-        Assert.Equal(TimeSystem.UT, actual.TimeSystem);
-        Assert.Equal(Time.Zero, actual.Offset);
-    }
+    public void Valid_ExactMatch() => Executor_Create.Valid_ExactMatch(GetService());
 
     private static IAngularEpochRangeFactory GetService() => DependencyInjection.GetRequiredService<IAngularEpochRangeFactory>();
-
-    private static IEpoch GetNullEpoch() => null!;
-    private static IEpoch GetValidStartEpoch() => new JulianDay(0);
-    private static IEpoch GetValidStopEpoch() => new JulianDay(1);
-    private static IEpoch GetEarlierStopEpoch() => new JulianDay(-1);
-
-    private static IStepSize GetNullStepSize() => null!;
-    private static IStepSize GetValidStepSize()
-    {
-        var factory = DependencyInjection.GetRequiredService<IUniformStepSizeFactory>();
-
-        return factory.Create(1);
-    }
 }
