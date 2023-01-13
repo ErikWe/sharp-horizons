6a143dc40532ea2d5e688efb993bf7bd6e62f8ef
ErikWe <erikwestberg7@gmail.com>
2023-01-13 01:19:37 +0100

    Reversed IEpochRangeFactory inheritance, such that IFixedEpochRangeFactory (and similar) is an IEpochRangeFactory

--- src/SharpHorizons.Query.Abstractions/Epoch/IAngularEpochRangeFactory.cs
+++ src/SharpHorizons.Query.Abstractions/Epoch/IAngularEpochRangeFactory.cs
@@ -9,7 +9,7 @@ using System;
 
 /// <summary>Handles construction of <see cref="IEpochRange"/> using the timespan between two <see cref="IEpoch"/>, with the <see cref="IStepSize"/> represented by the variable amount of time it takes for the position of some <see cref="ITarget"/> to change by some <see cref="Angle"/>, as seen from some <see cref="IOrigin"/>.</summary>
 /// <remarks>An <see cref="IEpochRange"/> of this type is only supported in queries for observational state.</remarks>
-public interface IAngularEpochRangeFactory
+public interface IAngularEpochRangeFactory : IEpochRangeFactory
 {
     /// <summary>Constructs a <see cref="IEpochRange"/> using the timespan between <paramref name="startEpoch"/> and <paramref name="stopEpoch"/>, with the <see cref="IStepSize"/> represented by the variable amount of time it takes for the position of some <see cref="ITarget"/> to change by <paramref name="deltaAngle"/>, as seen from some <see cref="IOrigin"/>.</summary>
     /// <param name="startEpoch"><inheritdoc cref="IEpochRange.StartEpoch" path="/summary"/></param>
