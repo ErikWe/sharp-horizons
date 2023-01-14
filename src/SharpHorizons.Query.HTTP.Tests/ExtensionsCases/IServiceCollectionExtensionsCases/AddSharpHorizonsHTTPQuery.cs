e17984cdd93a00919e77f1b7d179a00c4cf29e61
ErikWe <erikwestberg7@gmail.com>
2023-01-14 19:43:23 +0100

    Added .Query.HTTP.Tests project, with DI test-cases

---
+++ src/SharpHorizons.Query.HTTP.Tests/ExtensionsCases/IServiceCollectionExtensionsCases/AddSharpHorizonsHTTPQuery.cs
@@ -0,0 +1,33 @@
+﻿namespace SharpHorizons.Tests.QueryCases.ExtensionsCases.IServiceCollectionExtensionsCases;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using SharpHorizons.Extensions.Query.HTTP;
+using SharpHorizons.Query.Request.HTTP;
+
+using Xunit;
+
+public class AddSharpHorizonsHTTPQuery
+{
+    [Fact]
+    public void ReturnsSameInstance()
+    {
+        var expected = GetServiceCollection();
+
+        var actual = expected.AddSharpHorizonsHTTPQuery();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void UsingDefaultConfiguration()
+    {
+        var provider = GetServiceCollection().AddSharpHorizonsHTTPQuery().BuildServiceProvider();
+
+        var httpAddressProvider = provider.GetRequiredService<IHorizonsHTTPAddressProvider>();
+
+        Assert.Equal("https://ssd.jpl.nasa.gov/api/horizons.api", httpAddressProvider.HTTPAddress.ToString());
+    }
+
+    private static IServiceCollection GetServiceCollection() => new ServiceCollection();
+}
