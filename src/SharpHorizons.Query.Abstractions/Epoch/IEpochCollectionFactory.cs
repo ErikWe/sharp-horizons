aafb44272008079311a51450a6c8289925514f7f
ErikWe <erikwestberg7@gmail.com>
2023-01-18 01:53:30 +0100

    Allow EpochFormat, CalendarType etc to be specified through IEpochRange/CollectionFactory

--- src/SharpHorizons.Query.Abstractions/Epoch/IEpochCollectionFactory.cs
+++ src/SharpHorizons.Query.Abstractions/Epoch/IEpochCollectionFactory.cs
@@ -1,18 +1,48 @@
 ﻿namespace SharpHorizons.Query.Epoch;
 
+using SharpMeasures;
+
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 /// <summary>Handles construction of <see cref="IEpochCollection"/>.</summary>
 public interface IEpochCollectionFactory
 {
-    /// <summary>Constructs a <see cref="IEpochCollection"/> containing <paramref name="epochs"/>.</summary>
+    /// <summary>Constructs an <see cref="IEpochCollection"/> containing <paramref name="epochs"/>.</summary>
     /// <param name="epochs"><inheritdoc cref="IEpochCollection.Epochs" path="/summary"/></param>
     /// <exception cref="ArgumentNullException"/>
     public abstract IEpochCollection Create(IEnumerable<IEpoch> epochs);
 
-    /// <summary>Constructs a <see cref="IEpochCollection"/> containing <paramref name="epochs"/>.</summary>
+    /// <summary>Constructs an <see cref="IEpochCollection"/> containing <paramref name="epochs"/>.</summary>
     /// <param name="epochs"><inheritdoc cref="IEpochCollection.Epochs" path="/summary"/></param>
     /// <exception cref="ArgumentNullException"/>
     public abstract IEpochCollection Create(params IEpoch[] epochs);
+
+    /// <summary>Specifies the <see cref="IEpochSelection.Format"/> of the constructed <see cref="IEpochCollection"/>.</summary>
+    /// <param name="format"><inheritdoc cref="IEpochSelection.Format" path="/summary"/></param>
+    /// <returns>The same instance of <see cref="IEpochCollectionFactory"/>, so that calls can be chained.</returns>
+    /// <exception cref="ArgumentException"/>
+    /// <exception cref="InvalidEnumArgumentException"/>
+    public abstract IEpochCollectionFactory WithFormat(EpochFormat format);
+
+    /// <summary>Specifies the <see cref="IEpochSelection.Calendar"/> of the constructed <see cref="IEpochCollection"/>.</summary>
+    /// <param name="calendar"><inheritdoc cref="IEpochSelection.Calendar" path="/summary"/></param>
+    /// <returns>The same instance of <see cref="IEpochCollectionFactory"/>, so that calls can be chained.</returns>
+    /// <exception cref="ArgumentException"/>
+    /// <exception cref="InvalidEnumArgumentException"/>
+    public abstract IEpochCollectionFactory WithCalendar(CalendarType calendar);
+
+    /// <summary>Specifies the <see cref="IEpochSelection.TimeSystem"/> of the constructed <see cref="IEpochCollection"/>.</summary>
+    /// <param name="timeSystem"><inheritdoc cref="IEpochSelection.TimeSystem" path="/summary"/></param>
+    /// <returns>The same instance of <see cref="IEpochCollectionFactory"/>, so that calls can be chained.</returns>
+    /// <exception cref="ArgumentException"/>
+    /// <exception cref="InvalidEnumArgumentException"/>
+    public abstract IEpochCollectionFactory WithTimeSystem(TimeSystem timeSystem);
+
+    /// <summary>Specifies the <see cref="IEpochSelection.UTCOffset"/> of the constructed <see cref="IEpochCollection"/>.</summary>
+    /// <param name="utcOffset"><inheritdoc cref="IEpochSelection.UTCOffset" path="/summary"/></param>
+    /// <returns>The same instance of <see cref="IEpochCollectionFactory"/>, so that calls can be chained.</returns>
+    /// <exception cref="ArgumentException"/>
+    public abstract IEpochCollectionFactory WithUTCOffset(Time utcOffset);
 }
--- src/SharpHorizons.Query.Abstractions/Epoch/IEpochRangeFactory.cs
+++ src/SharpHorizons.Query.Abstractions/Epoch/IEpochRangeFactory.cs
@@ -1,15 +1,46 @@
 ﻿namespace SharpHorizons.Query.Epoch;
 
+using SharpMeasures;
+
 using System;
+using System.ComponentModel;
 
 /// <summary>Handles construction of <see cref="IEpochRange"/> using the timespan between two <see cref="IEpoch"/>, and some <see cref="IStepSize"/>.</summary>
 public interface IEpochRangeFactory
 {
-    /// <summary>Constructs a <see cref="IEpochRange"/> using the timespan between <paramref name="startEpoch"/> and <paramref name="stopEpoch"/>, and some <paramref name="stepSize"/>.</summary>
+    /// <summary>Constructs an <see cref="IEpochRange"/> using the timespan between <paramref name="startEpoch"/> and <paramref name="stopEpoch"/>, and some <paramref name="stepSize"/>.</summary>
     /// <param name="startEpoch"><inheritdoc cref="IEpochRange.StartEpoch" path="/summary"/></param>
     /// <param name="stopEpoch"><inheritdoc cref="IEpochRange.StopEpoch" path="/summary"/></param>
     /// <param name="stepSize"><inheritdoc cref="IEpochRange.StepSize" path="/summary"/></param>
+    /// <returns>The constructed <see cref="IEpochRange"/>.</returns>
     /// <exception cref="ArgumentException"/>
     /// <exception cref="ArgumentNullException"/>
     public abstract IEpochRange Create(IEpoch startEpoch, IEpoch stopEpoch, IStepSize stepSize);
+
+    /// <summary>Specifies the <see cref="IEpochSelection.Format"/> of the constructed <see cref="IEpochRange"/>.</summary>
+    /// <param name="format"><inheritdoc cref="IEpochSelection.Format" path="/summary"/></param>
+    /// <returns>The same instance of <see cref="IEpochRangeFactory"/>, so that calls can be chained.</returns>
+    /// <exception cref="ArgumentException"/>
+    /// <exception cref="InvalidEnumArgumentException"/>
+    public abstract IEpochRangeFactory WithFormat(EpochFormat format);
+
+    /// <summary>Specifies the <see cref="IEpochSelection.Calendar"/> of the constructed <see cref="IEpochRange"/>.</summary>
+    /// <param name="calendar"><inheritdoc cref="IEpochSelection.Calendar" path="/summary"/></param>
+    /// <returns>The same instance of <see cref="IEpochRangeFactory"/>, so that calls can be chained.</returns>
+    /// <exception cref="ArgumentException"/>
+    /// <exception cref="InvalidEnumArgumentException"/>
+    public abstract IEpochRangeFactory WithCalendar(CalendarType calendar);
+
+    /// <summary>Specifies the <see cref="IEpochSelection.TimeSystem"/> of the constructed <see cref="IEpochRange"/>.</summary>
+    /// <param name="timeSystem"><inheritdoc cref="IEpochSelection.TimeSystem" path="/summary"/></param>
+    /// <returns>The same instance of <see cref="IEpochRangeFactory"/>, so that calls can be chained.</returns>
+    /// <exception cref="ArgumentException"/>
+    /// <exception cref="InvalidEnumArgumentException"/>
+    public abstract IEpochRangeFactory WithTimeSystem(TimeSystem timeSystem);
+
+    /// <summary>Specifies the <see cref="IEpochSelection.UTCOffset"/> of the constructed <see cref="IEpochRange"/>.</summary>
+    /// <param name="utcOffset"><inheritdoc cref="IEpochSelection.UTCOffset" path="/summary"/></param>
+    /// <returns>The same instance of <see cref="IEpochRangeFactory"/>, so that calls can be chained.</returns>
+    /// <exception cref="ArgumentException"/>
+    public abstract IEpochRangeFactory WithUTCOffset(Time utcOffset);
 }
